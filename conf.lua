8e91fdad4e66d5910ef1b8aeb4ace24efe6c3cc9
ebernerd <micromacroeric@gmail.com>
2016-08-28 23:18:39 -0400

    fix conf

--- conf.lua
+++ conf.lua
@@ -1,11 +1,11 @@
 function love.conf(t)
     t.identity = "Untold Secrets"                    -- The name of the save directory (string)
     t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
-    t.console = true             -- Attach a console (boolean, Windows only)
+    t.console = false             -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
  
-    t.window.title = "The Materialistic Shift"-- The window title (string)
+    t.window.title = "The Stones of Kelzi"-- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
     t.window.width = 1280             -- The window width (number)
     t.window.height = 720             -- The window height (number)
